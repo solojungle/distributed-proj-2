cea16d9cf37ef4dedd1cf19534a0e83e3fe1fe52
solojungle <ali.awari.0@gmail.com>
2020-03-09 12:24:40 -0400

    Added comments and initial code

--- MAPR/src/ds/hdfs/INameNode.java
+++ MAPR/src/ds/hdfs/INameNode.java
@@ -1,30 +1,37 @@
 package ds.hdfs;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface INameNode extends Remote{
+/**
+ * 1. Interface is public
+ * 2. Extends the Remote interface
+ * 3. Defines methods that will be accessed remotely
+ * 4. Each method must throw a RemoteException
+ */
+public interface INameNode extends Remote {
 
-	/* Method to open a file given file name with read-write flag*/
-	byte[] openFile(byte[] inp) throws RemoteException;
-	
-	byte[] closeFile(byte[] inp ) throws RemoteException;
-	
-	/* Method to get block locations given an array of block numbers */
-	byte[] getBlockLocations(byte[] inp ) throws RemoteException;
-	
-	/* Method to assign a block which will return the replicated block locations */
-	byte[] assignBlock(byte[] inp ) throws RemoteException;
-	
-	/* List the file names (no directories needed for current implementation */
-	byte[] list(byte[] inp ) throws RemoteException;
+    /* Method to open a file given file name with read-write flag*/
+    byte[] openFile(byte[] inp) throws RemoteException;
+
+    byte[] closeFile(byte[] inp) throws RemoteException;
+
+    /* Method to get block locations given an array of block numbers */
+    byte[] getBlockLocations(byte[] inp) throws RemoteException;
+
+    /* Method to assign a block which will return the replicated block locations */
+    byte[] assignBlock(byte[] inp) throws RemoteException;
+
+    /* List the file names (no directories needed for current implementation */
+    byte[] list(byte[] inp) throws RemoteException;
 	
 	/*
 		Datanode <-> Namenode interaction methods
 	*/
-	
-	/* Get the status for blocks */
-	byte[] blockReport(byte[] inp ) throws RemoteException;
-	
-	/* Heartbeat messages between NameNode and DataNode */
-	byte[] heartBeat(byte[] inp ) throws RemoteException;
+
+    /* Get the status for blocks */
+    byte[] blockReport(byte[] inp) throws RemoteException;
+
+    /* Heartbeat messages between NameNode and DataNode */
+    byte[] heartBeat(byte[] inp) throws RemoteException;
 }
--- MAPR/src/ds/hdfs/NameNode.java
+++ MAPR/src/ds/hdfs/NameNode.java
@@ -1,184 +1,224 @@
 package ds.hdfs;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.UnknownHostException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.*;
-
 import ds.hdfs.hdfsformat.*;
 
-public class NameNode implements INameNode{
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
-	protected Registry serverRegistry;
-	
-	public NameNode(String addr,int p, String nn)
-	{
-		ip = addr;
-		port = p;
-		name = nn;
-	}
-	
-	public static class DataNode
-	{
-		String ip;
-		int port;
-		String serverName;
-		public DataNode(String addr,int p,String sname)
-		{
-			ip = addr;
-			port = p;
-			serverName = sname;
-		}
-	}
-	
-	public static class FileInfo
-	{
-		String filename;
-		int filehandle;
-		boolean writemode;
-		ArrayList<Integer> Chunks;
-		public FileInfo(String name, int handle, boolean option)
-		{
-			filename = name;
-			filehandle = handle;
-			writemode = option;
-			Chunks = new ArrayList<Integer>();
-		}
-	}
-	/* Method to open a file given file name with read-write flag*/
-	
-	boolean findInFilelist(int fhandle)
-	{
-	}
-	
-	public void printFilelist()
-	{
-	}
-	
-	public byte[] openFile(byte[] inp) throws RemoteException
-	{
-		try
-		{
-		}
-		catch (Exception e) 
-		{
-			System.err.println("Error at " + this.getClass() + e.toString());
-			e.printStackTrace();
-			response.setStatus(-1);
-		}
-		return response.toByteArray();
-	}
-	
-	public byte[] closeFile(byte[] inp ) throws RemoteException
-	{
-		try
-		{
-		}
-		catch(Exception e)
-		{
-			System.err.println("Error at closefileRequest " + e.toString());
-			e.printStackTrace();
-			response.setStatus(-1);
-		}
-		
-		return response.build().toByteArray();
-	}
-	
-	public byte[] getBlockLocations(byte[] inp ) throws RemoteException
-	{
-		try
-		{
-		}
-		catch(Exception e)
-		{
-			System.err.println("Error at getBlockLocations "+ e.toString());
-			e.printStackTrace();
-			response.setStatus(-1);
-		}		
-		return response.build().toByteArray();
-	}
-	
-	
-	public byte[] assignBlock(byte[] inp ) throws RemoteException
-	{
-		try
-		{
-		}
-		catch(Exception e)
-		{
-			System.err.println("Error at AssignBlock "+ e.toString());
-			e.printStackTrace();
-			response.setStatus(-1);
-		}
-		
-		return response.build().toByteArray();
-	}
-		
-	
-	public byte[] list(byte[] inp ) throws RemoteException
-	{
-		try
-		{
-		}catch(Exception e)
-		{
-			System.err.println("Error at list "+ e.toString());
-			e.printStackTrace();
-			response.setStatus(-1);
-		}
-		return response.build().toByteArray();
-	}
-	
-	// Datanode <-> Namenode interaction methods
-		
-	public byte[] blockReport(byte[] inp ) throws RemoteException
-	{
-		try
-		{
-		}
-		catch(Exception e)
-		{
-			System.err.println("Error at blockReport "+ e.toString());
-			e.printStackTrace();
-			response.addStatus(-1);
-		}
-		return response.build().toByteArray();
-	}
-	
-	
-	
-	public byte[] heartBeat(byte[] inp ) throws RemoteException
-	{
-		return response.build().toByteArray();
-	}
-	
-	public void printMsg(String msg)
-	{
-		System.out.println(msg);		
-	}
-	
-	public static void main(String[] args) throws InterruptedException, NumberFormatException, IOException
-	{
-	}
-	
+public class NameNode implements INameNode {
+
+    /**
+     *
+     */
+    protected Registry serverRegistry;
+
+    /**
+     * @param addr
+     * @param p
+     * @param nn
+     */
+    public NameNode(String addr, int p, String nn) {
+        ip = addr;
+        port = p;
+        name = nn;
+    }
+
+    /**
+     * Entry point to instantiate the NameNode server
+     *
+     * @param args
+     * @throws InterruptedException
+     * @throws NumberFormatException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws InterruptedException, NumberFormatException, IOException {
+
+
+        // Grab IP Address + Port
+
+        int port = 10;
+        String addr = "127";
+
+
+        /* Create an instance of the NameNode server */
+        NameNode server = new NameNode("1asdads23", 1, "asd");
+
+        /* Register the instance with the name server (rmiregisty) */
+        Naming.rebind("NameNode", server);
+
+        System.out.println("Server is running on port: " + port);
+    }
+
+    /**
+     * Searches the file-list for a file
+     *
+     * @param fhandle
+     * @return bool
+     */
+    boolean findInFilelist(int fhandle) {
+        return false;
+    }
+
+    /**
+     * Prints the entirety of file-list
+     */
+    public void printFilelist() {
+    }
+
+    /**
+     * Method to open a file given file name with read-write flag
+     *
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] openFile(byte[] inp) throws RemoteException {
+        ResponseBuilder response = Response.ok();
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at " + getClass() + e.toString());
+            e.printStackTrace();
+            response.setStatus(-1);
+        }
+        return response.toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] closeFile(byte[] inp) throws RemoteException {
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at closefileRequest " + e.toString());
+            e.printStackTrace();
+            response.setStatus(-1);
+        }
+
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] getBlockLocations(byte[] inp) throws RemoteException {
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at getBlockLocations " + e.toString());
+            e.printStackTrace();
+            response.setStatus(-1);
+        }
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] assignBlock(byte[] inp) throws RemoteException {
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at AssignBlock " + e.toString());
+            e.printStackTrace();
+            response.setStatus(-1);
+        }
+
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] list(byte[] inp) throws RemoteException {
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at list " + e.toString());
+            e.printStackTrace();
+            response.setStatus(-1);
+        }
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] blockReport(byte[] inp) throws RemoteException {
+        try {
+        } catch (Exception e) {
+            System.err.println("Error at blockReport " + e.toString());
+            e.printStackTrace();
+            response.addStatus(-1);
+        }
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param inp
+     * @return
+     * @throws RemoteException
+     */
+    public byte[] heartBeat(byte[] inp) throws RemoteException {
+        return response.build().toByteArray();
+    }
+
+    /**
+     * @param msg
+     */
+    public void printMsg(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     *
+     */
+    public static class DataNode {
+        String ip;
+        int port;
+        String serverName;
+
+        /**
+         * @param addr
+         * @param p
+         * @param sname
+         */
+        public DataNode(String addr, int p, String sname) {
+            ip = addr;
+            port = p;
+            serverName = sname;
+        }
+    }
+
+    /**
+     *
+     */
+    public static class FileInfo {
+        String filename;
+        int filehandle;
+        boolean writemode;
+        ArrayList<Integer> Chunks;
+
+        /**
+         * @param name
+         * @param handle
+         * @param option
+         */
+        public FileInfo(String name, int handle, boolean option) {
+            filename = name;
+            filehandle = handle;
+            writemode = option;
+            Chunks = new ArrayList<Integer>();
+        }
+    }
+
 }
