1f0f5a431504002ff6587be40d468d235f8eb7c3
Michelle Wu <wuwmichelle@gmail.com>
2020-03-27 16:21:09 -0400

    added -clean option to run.sh

--- MAPR/run.sh
+++ MAPR/run.sh
@@ -12,4 +12,7 @@ then
 elif [ $1 = '-nameNode' ]
 then
 	java -cp src/bin:src/protobuf-java-3.11.4.jar:src/json-simple-1.1.1.jar ds.hdfs.NameNode
+elif [ $1 = '-clean' ]
+then
+	rm -rf src/bin/* 
 fi
