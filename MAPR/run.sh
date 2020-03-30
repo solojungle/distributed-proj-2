3324806159b608998671da147d63531482c3a512
Michelle Wu <wuwmichelle@gmail.com>
2020-03-30 16:40:48 -0400

    removed extra args from run.sh

--- MAPR/run.sh
+++ MAPR/run.sh
@@ -5,10 +5,10 @@ then
 	javac -d src/bin/ -cp src/ds/hdfs:src/protobuf-java-3.11.4.jar:src/json-simple-1.1.1.jar src/ds/hdfs/*.java
 elif [ $1 = '-client' ]
 then
-	java -cp src/bin:src/protobuf-java-3.11.4.jar ds.hdfs.Client $2
+	java -cp src/bin:src/protobuf-java-3.11.4.jar ds.hdfs.Client
 elif [ $1 = '-dataNode' ]
 then
-	java -cp src/bin:src/protobuf-java-3.11.4.jar ds.hdfs.DataNode $2
+	java -cp src/bin:src/protobuf-java-3.11.4.jar ds.hdfs.DataNode
 elif [ $1 = '-nameNode' ]
 then
 	java -cp src/bin:src/protobuf-java-3.11.4.jar:src/json-simple-1.1.1.jar ds.hdfs.NameNode
